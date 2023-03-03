1faaae67cbe36c72644a20c3c0619d30b9ab8686
geraltyen <girireddyanna@gmail.com>
2023-03-03 13:41:49 +0530

    homepage done

--- bird/src/App.tsx
+++ bird/src/App.tsx
@@ -2,12 +2,16 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
+import Homepage from './components/Homepage';
+import Footer from './components/Footer';
 
 
 function App() {
   return (
     <>
       <Navbar/>
+      <Homepage/>
+      <Footer/>
     </>
   );
 }
