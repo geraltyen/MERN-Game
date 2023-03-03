5a82142ec15b6ff22a38013d881a35c4347a9a8f
sukanta255 <sukantap872@gmail.com>
2023-03-03 15:50:12 +0530

    Button static part added

--- bird/src/App.tsx
+++ bird/src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
+import Button from "./components/Button";
 
 
 function App() {
   return (
     <>
       <Navbar/>
+      <Button/>
     </>
   );
 }
