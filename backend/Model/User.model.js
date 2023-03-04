76b28e968042ef7611e5de186c080271bbfd056e
Vikram Navik <vikramnavik98@gmail.com>
2023-03-04 14:48:35 +0530

    req added

--- backend/Model/User.model.js
+++ backend/Model/User.model.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose");
 
 const userSchema = mongoose.Schema({
-    fname:{type:String, trim:true},
-    lname:{type:String, trim:true},
+    fname:{type:String, required:true, trim:true},
+    lname:{type:String, required:true, trim:true},
     email:{type:String, required:true, trim:true},
     password:{type:String, required:true, trim:true},
     score:{type:Number, required:true}
